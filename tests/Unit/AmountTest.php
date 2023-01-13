fd53ae67699d62905b79729a27f71a851c76c4d9
Vladislavs Poznaks <vladislavs.poznaks@gmail.com>
2023-01-13 12:28:30 +0200

    Added amount unit test

---
+++ tests/Unit/AmountTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Amount;
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+
+class AmountTest extends TestCase
+{
+    /**
+     * @test
+     * @dataProvider getInvalidAmounts
+     */
+    public function it_throws_an_exception_if_invalid_amount($amount)
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        new Amount($amount);
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_be_created_with_a_factory_method()
+    {
+        $amount = 100;
+
+        $this->assertEquals($amount, Amount::make($amount)->getAmount());
+    }
+
+    public function getInvalidAmounts()
+    {
+        return [
+            [-10],
+            [0],
+        ];
+    }
+}
