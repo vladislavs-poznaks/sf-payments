d32940f939e4b4581f4edb54be8a6189c9242115
Vladislavs Poznaks <vladislavs.poznaks@gmail.com>
2023-01-14 21:01:47 +0200

    Added client side http error codes

--- src/Http/Response.php
+++ src/Http/Response.php
@@ -7,6 +7,9 @@ class Response
     const HTTP_OK = 200;
     const HTTP_CREATED = 201;
 
+    const HTTP_BAD_REQUEST = 400;
+    const HTTP_CONFLICT = 409;
+
     public static function json(array $data = [], $httpCode = self::HTTP_OK): string
     {
         http_response_code($httpCode);
