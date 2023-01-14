ae4a4e56fd31eb3ae677d816d10eace294d0ff04
Vladislavs Poznaks <vladislavs.poznaks@gmail.com>
2023-01-14 14:47:58 +0200

    Added doctrine orm mapping types for carbon, amount

---
+++ src/Types/AmountType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Types;
+
+use App\Models\ValueObjects\Amount;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\Type;
+
+class AmountType extends Type
+{
+    const NAME = 'amount';
+
+    public function getSQLDeclaration(array $column, AbstractPlatform $platform)
+    {
+        // TODO: Implement getSQLDeclaration() method.
+    }
+
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        return Amount::make($value);
+    }
+
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        return $value->getAmount();
+    }
+
+    public function getName(): string
+    {
+        return self::NAME;
+    }
+}
---
+++ src/Types/CarbonType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Types;
+
+use App\Models\ValueObjects\Amount;
+use Carbon\Carbon;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\Type;
+
+class CarbonType extends Type
+{
+    const NAME = 'carbon';
+
+    public function getSQLDeclaration(array $column, AbstractPlatform $platform)
+    {
+        // TODO: Implement getSQLDeclaration() method.
+    }
+
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        return Carbon::parse($value);
+    }
+
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        return $value->toDatetimeString();
+    }
+
+    public function getName(): string
+    {
+        return self::NAME;
+    }
+}
