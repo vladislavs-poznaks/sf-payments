6718c5940212befbe19024b053040f699b50cfb8
Vladislavs Poznaks <vladislavs.poznaks@gmail.com>
2023-01-14 21:04:09 +0200

    Added custom validation rule for payment date to bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -7,7 +7,18 @@ require_once 'vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
+// Doctrine ORM mapping types
 Type::addType(\Ramsey\Uuid\Doctrine\UuidType::NAME, \Ramsey\Uuid\Doctrine\UuidType::class);
 Type::addType(\App\Types\CarbonType::NAME, \App\Types\CarbonType::class);
 Type::addType(\App\Types\AmountType::NAME, \App\Types\AmountType::class);
 Type::addType(\App\Types\LoanNumberType::NAME, \App\Types\LoanNumberType::class);
+
+// Custom validation
+Valitron\Validator::addRule('paymentDateFormat', function($field, $value, array $params, array $fields) {
+    try {
+        \Carbon\Carbon::createFromFormat('c', $value);
+        return true;
+    } catch (\Carbon\Exceptions\InvalidFormatException) {
+        return false;
+    }
+}, "Incorrect {field} format");
