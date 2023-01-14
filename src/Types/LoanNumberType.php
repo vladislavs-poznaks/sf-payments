8d2faaea6577dc52dcc53aadcb673e0af7a8548e
Vladislavs Poznaks <vladislavs.poznaks@gmail.com>
2023-01-14 15:27:03 +0200

    Added loan number doctrine orm mapping type

---
+++ src/Types/LoanNumberType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Types;
+
+use App\Models\ValueObjects\LoanNumber;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\Type;
+
+class LoanNumberType extends Type
+{
+    const NAME = 'loan_number';
+
+    public function getSQLDeclaration(array $column, AbstractPlatform $platform)
+    {
+        // TODO: Implement getSQLDeclaration() method.
+    }
+
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        return LoanNumber::make($value);
+    }
+
+    public function getName(): string
+    {
+        return self::NAME;
+    }
+}
