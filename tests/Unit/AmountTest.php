cb31eb71e6fe3802c2233b7b647f192cb5b15e6e
Vladislavs Poznaks <vladislavs.poznaks@gmail.com>
2023-01-15 17:13:12 +0200

    Added test cases for comparision, subtraction operations in amount

--- tests/Unit/AmountTest.php
+++ tests/Unit/AmountTest.php
@@ -29,11 +29,35 @@ class AmountTest extends TestCase
         $this->assertEquals($amount, Amount::make($amount)->getAmount());
     }
 
+    /**
+     * @test
+     */
+    public function it_can_compare_two_amounts()
+    {
+        $amountOfFive = Amount::make(5);
+        $amountOfTen = Amount::make(10);
+
+        $this->assertTrue($amountOfFive->lt($amountOfTen));
+        $this->assertFalse($amountOfTen->lt($amountOfFive));
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_create_a_subtracted_amount()
+    {
+        $amountOfThree = Amount::make(3);
+        $amountOfTen = Amount::make(10);
+
+        $amount = $amountOfTen->subtract($amountOfThree);
+
+        $this->assertEquals(7, $amount->getAmount());
+    }
+
     public function getInvalidAmounts()
     {
         return [
             [-10],
-            [0],
         ];
     }
 }
