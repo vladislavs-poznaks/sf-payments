e36ab3a0d00f4cff6bb6d3e3cc24efc934d82b4d
Vladislavs Poznaks <vladislavs.poznaks@gmail.com>
2023-01-15 11:12:00 +0200

    Added payments get by date method, added store method, updated get by ref id method

--- src/Repositories/PaymentsDatabaseRepository.php
+++ src/Repositories/PaymentsDatabaseRepository.php
@@ -2,19 +2,46 @@
 
 namespace App\Repositories;
 
+use App\Models\Payment;
+use Carbon\Carbon;
+use Ramsey\Uuid\UuidInterface;
+
 class PaymentsDatabaseRepository extends DatabaseRepository
 {
-    public function getById(int $id)
+    public function getByRefId(UuidInterface|string $refId): ?Payment
     {
-        $query = $this->getConnection()->createQueryBuilder();
+        $query = $this->entityManager->createQueryBuilder();
 
         return $query
-            ->select('id')
-            ->from('payments')
-            ->where('id = :id')
-            ->setParameters([
-                'id' => $id
-            ])
-            ->fetchAssociative();
+            ->select('p')
+            ->from(Payment::class, 'p')
+            ->where('p.refId = :refId')
+            ->setParameter('refId', $refId)
+            ->getQuery()
+            ->getOneOrNullResult();
+    }
+
+    public function getByDate(Carbon $date)
+    {
+        $query = $this->entityManager->createQueryBuilder();
+
+        return $query
+            ->select('p')
+            ->from(Payment::class, 'p')
+            ->where('p.paymentDate LIKE :date')
+            ->setParameter('date', $date->format('Y-m-d') . '%')
+            ->getQuery()
+            ->getResult();
+    }
+
+    public function store(Payment $payment)
+    {
+        $this->entityManager->persist($payment);
+
+        $this->entityManager->flush();
+
+        $this->entityManager->refresh($payment);
+
+        return $payment;
     }
 }
